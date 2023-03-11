dc89e0214fdd6ed1397d9640738ed278c54516ac
Kirill Karpinovsky <karpinovsky@gmail.com>
2023-03-11 18:08:22 +0300

    Add before_extract block

--- lib/bestchange/api.rb
+++ lib/bestchange/api.rb
@@ -10,21 +10,21 @@ module Bestchange
   module Api
     extend self
 
-    def get_files(filenames)
+    def get_files(filenames, &before_extract)
       Timeout.timeout(Bestchange.configuration.timeout) do
-        fetch_files(filenames)
+        fetch_files(filenames, &before_extract)
       end
     end
 
     private
 
-    def fetch_files(filenames)
+    def fetch_files(filenames, &before_extract)
       response = make_request
 
       archive = Tempfile.new(ARCHIVE_DATA_FILENAME, encoding: ARCHIVE_DATA_ENCODING)
       archive.write(response)
 
-      extract_files(archive, filenames)
+      extract_files(archive, filenames, &before_extract)
     ensure
       archive.close if defined?(archive)
     end
@@ -33,12 +33,14 @@ module Bestchange
       Net::HTTP.get(URI(BASE_URI))
     end
 
-    def extract_files(archive, filenames)
+    def extract_files(archive, filenames, &before_extract)
       Zip::File.open(archive) do |zip_file|
         filenames.map do |filename|
           entry = zip_file.find_entry(filename)
 
           pathname = Pathname.new(Bestchange.configuration.dir).join(filename)
+
+          before_extract&.call(filename, pathname)
 
           zip_file.extract(entry, pathname)
 
